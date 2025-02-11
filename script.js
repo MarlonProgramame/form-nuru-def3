cd296d70e489fc1756f9fb6f00c0f33a7673dff3
sistemasNuru3na <sistemasnuruena@gmail.com>
2025-02-11 12:22:58 -0500

    Update script.js
    input prueba

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var estado_llanta_del_der_malo = document.getElementById('estado_llanta_del_der_malo');
 
 estado_llanta_del_der_malo.addEventListener('click', function(){
-    var obs_estado_llanta_del_der_malo = document.createElement('input');
+    var inputPrueba = document.createElement('input');
+    inputPrueba.type = "text";
+    table.appendChild(inputPrueba);
 });
