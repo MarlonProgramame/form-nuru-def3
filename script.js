390f5c80f28f4f265c679e03879f291f69dbd1d9
sistemasNuru3na <sistemasnuruena@gmail.com>
2025-02-11 12:11:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var estado_llanta_del_der_malo = document.getElementById('estado_llanta_del_der_malo');
 
 estado_llanta_del_der_malo.addEventListener('click', function(){
-    this.alert("Hola");
+    alert("Hola");
 });
